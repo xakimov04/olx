6f6e47a2d39660ee7cca1ad3976c638aea02fd3e
xakimov04 <mardonhakimov0404@gmail.com>
2024-05-16 19:50:26 +0500

    first_commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# olx
+
+![video](assets/readme/1.mp4)
---
+++ lib/pages/home_page.dart
@@ -0,0 +1,168 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:olx/data/list_data.dart';
+import 'package:olx/pages/galeriya_page.dart';
+import 'package:olx/pages/plitka_page.dart';
+import 'package:olx/pages/sipiska_page.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int select = 2;
+  final List<Widget> pages = const [
+    GaleriyaPage(),
+    SipiskaPage(),
+    PlitkaPage()
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          "МЫ НАШЛИ БОЛЕЕ 1 000 ОБЪЯВЛЕНИЙ",
+          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15),
+        ),
+        actions: [
+          Row(
+            children: [
+              IconButton(
+                onPressed: () {
+                  setState(() {
+                    Data().data.reversed;
+                  });
+                },
+                icon: const Icon(CupertinoIcons.arrow_up_arrow_down),
+              ),
+              PopupMenuButton(
+                onSelected: (value) {
+                  setState(() {
+                    select = value;
+                  });
+                },
+                icon: select == 0
+                    ? Image.asset("assets/icons/menu_galeriya.png")
+                    : select == 1
+                        ? const Icon(CupertinoIcons.list_bullet)
+                        : Image.asset(
+                            "assets/icons/menu.png",
+                            width: 30,
+                            height: 30,
+                          ),
+                popUpAnimationStyle: AnimationStyle(
+                  curve: Curves.easeInCirc,
+                  duration: const Duration(milliseconds: 500),
+                ),
+                itemBuilder: (context) {
+                  return [
+                    PopupMenuItem(
+                      value: 0,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            "Галерея",
+                            style: TextStyle(
+                                fontWeight: select == 0
+                                    ? FontWeight.bold
+                                    : FontWeight.normal),
+                          ),
+                          if (select == 0)
+                            Icon(
+                              Icons.done,
+                              color: Colors.greenAccent.shade400,
+                            ),
+                        ],
+                      ),
+                    ),
+                    PopupMenuItem(
+                      value: 1,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            "Список",
+                            style: TextStyle(
+                                fontWeight: select == 1
+                                    ? FontWeight.bold
+                                    : FontWeight.normal),
+                          ),
+                          if (select == 1)
+                            Icon(
+                              Icons.done,
+                              color: Colors.greenAccent.shade400,
+                            ),
+                        ],
+                      ),
+                    ),
+                    PopupMenuItem(
+                      value: 2,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            "Плитка",
+                            style: TextStyle(
+                                fontWeight: select == 2
+                                    ? FontWeight.bold
+                                    : FontWeight.normal),
+                          ),
+                          if (select == 2)
+                            Icon(
+                              Icons.done,
+                              color: Colors.greenAccent.shade400,
+                            ),
+                        ],
+                      ),
+                    ),
+                  ];
+                },
+              ),
+            ],
+          )
+        ],
+      ),
+      floatingActionButton: Padding(
+        padding: const EdgeInsets.only(left: 50),
+        child: Align(
+          alignment: Alignment.bottomCenter,
+          child: InkWell(
+            onTap: () {},
+            child: Container(
+              width: 200,
+              height: 50,
+              decoration: BoxDecoration(
+                color: Colors.blue.shade50,
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(6.0),
+                child: Container(
+                  color: Colors.blue,
+                  child: const Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Icon(
+                        Icons.favorite_border,
+                        color: Colors.white,
+                      ),
+                      Text(
+                        "Сохранить поиск",
+                        style: TextStyle(color: Colors.white, fontSize: 18),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+      body: pages[select],
+    );
+  }
+}
